5d6a272584389ffc83198b33bae460fb2fa251d9
AbouCloud <Abouzari6839@gmail.com>
2025-02-16 00:59:00 +0000

    End of ABOUT US and some update like font-family on body
    Abzar

--- FormulaireRET.php
+++ FormulaireRET.php
@@ -149,7 +149,7 @@ session_start();
                   </li>
               </ul>
               <li class="nav-item me-3 ">
-                <a class="nav-link text-uppercase" href="Acceuil.php">A propos</a>
+                <a class="nav-link text-uppercase" href="apropos.php">A propos</a>
               </li>
               <li class="nav-item me-3 ">
                 <a class="nav-link active text-uppercase" href="Login.php">Login</a>
--- Login.php
+++ Login.php
@@ -34,7 +34,7 @@
                   </li>
               </ul>
               <li class="nav-item me-3 ">
-                <a class="nav-link text-uppercase" href="Acceuil.php">A propos</a>
+                <a class="nav-link text-uppercase" href="apropos.php">A propos</a>
               </li>
               <li class="nav-item me-3 ">
                 <a class="nav-link active text-uppercase" href="Login.php">Login</a>
